532595a56881f3be86f4f9b7dae795a0e3136c9f
unknown <sujaigorai544@gmail.com>
2023-06-26 22:02:33 +0530

    update menu animation

--- code/src/lib/component/Nav.svelte
+++ code/src/lib/component/Nav.svelte
@@ -1,14 +1,15 @@
-<script>
+<script lang="ts">
   import Bar from './Bar.svelte'
 
   let isOpen = false
   $: top = isOpen
   $: middle = isOpen
   $: bottom = isOpen
+  $: hide = !isOpen
 </script>
 
 <button
-  class="col-start-1 col-end-13 mt-8 h-10 justify-self-center   md:hidden"
+  class="col-start-1 col-end-13 mt-8 h-10 justify-self-center md:hidden"
   on:click={() => (isOpen = !isOpen)}>
   <svg
     height="100%"
@@ -29,7 +30,8 @@
 </button>
 
 <nav
-  class="col-start-1 col-end-13  flex flex-col items-center  md:mt-auto   md:flex-row md:gap-8 md:justify-self-center  md:pt-14">
+  class="col-start-1 col-end-13 flex h-40  flex-col items-center  overflow-hidden transition-all duration-300 md:mt-auto md:h-auto  md:flex-row md:gap-8 md:justify-self-center md:pt-14 md:transition-none"
+  class:hide>
   <a href="/" class="para">HOME</a>
   <Bar className="my-1 md:hidden" />
   <a href="/" class="para">SHOP</a>
@@ -42,32 +44,49 @@
 </nav>
 
 <style>
-  svg > * {
-    transition: all 0.3s;
+  @media (max-width: 768px) {
+    svg {
+      animation: ping 1s alternate infinite;
+    }
+
+    .top {
+      transform-box: fill-box;
+      transform-origin: left center;
+      rotate: 36deg;
+      scale: 1.2 1;
+      transition: all 0.3s;
+    }
+
+    .middle {
+      transform-box: fill-box;
+      transform-origin: center center;
+      scale: 1.9 9;
+      opacity: 0;
+      border-radius: 100%;
+      transition: all 1.5s;
+    }
+
+    .bottom {
+      transform-box: fill-box;
+      transform-origin: left center;
+      rotate: -36deg;
+      scale: 1.2 1;
+      transition: all 0.3s;
+    }
+
+    .hide {
+      height: 0;
+    }
   }
 
-  .top {
-    transform-box: fill-box;
-    transform-origin: left center;
-    rotate: 36deg;
-    scale: 1.2 1;
-    transition: all 0.3s;
-  }
-
-  .middle {
-    transform-box: fill-box;
-    transform-origin: center center;
-    scale: 1.9 9;
-    opacity: 0;
-    border-radius: 100%;
-    transition: all 1.5s;
-  }
-
-  .bottom {
-    transform-box: fill-box;
-    transform-origin: left center;
-    rotate: -36deg;
-    scale: 1.2 1;
-    transition: all 0.3s;
+  @keyframes ping {
+    from {
+      scale: 0.9;
+      opacity: 0.5;
+    }
+    top {
+      scale: 1;
+      opacity: 1;
+    }
   }
 </style>
