7438b4225fe7c9ecb8aca3239fe7f7b66ee127a2
unknown <sujaigorai544@gmail.com>
2023-06-28 21:39:03 +0530

    update

--- code/svelte.config.js
+++ code/svelte.config.js
@@ -1,7 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite'
-// import adapter from '@sveltejs/adapter-cloudflare'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-cloudflare'
+// import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,13 +14,12 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: true,
-      strict: true
+
+      // pages: 'build',
+      // assets: 'build',
+      // fallback: undefined,
+      // precompress: true,
+      // strict: true
     })
   }
 }
